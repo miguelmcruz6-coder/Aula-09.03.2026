c2096f4b54734d0a4a2613d5f7b21433fa0ad932
Miguel Miyaki da Cruz - SENAI <miguel.m.cruz6@aluno.senai.br>
2026-03-10 10:56:29 -0300

    Refactor DOM manipulation exercises in script.js

--- script.js
+++ script.js
@@ -87,30 +87,184 @@ function adicionar(){
 
 // Exercício 5
 
-function alterarCor(){
-    let texto = document.getElementById("cor").style.color
-    if (texto === "yellow" || "black"){
-        texto = "purple"
-        console.log(texto)
-    }
-    else{
-        texto = "yellow"
-        console.log(texto)
-    }
+let texto = document.getElementById("cor").style.color
+function alterarRed(){
+    texto = decisaoRed(texto)
     document.getElementById("cor").style.color = texto
 }
 
+function alterarBlue(){
+    texto = decisaoBlue(texto)
+    document.getElementById("cor").style.color = texto
+}
+
+function decisaoRed(cor){
+    if (cor !== "red"){
+        cor = "red"
+    } else {
+        cor = "black"
+    }
+    return cor
+}
+
+function decisaoBlue(cor){
+    if (cor !== "blue"){
+        cor = "blue"
+    } else {
+        cor = "black"
+    }
+    return cor
+}
 
 
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+// Multiplicação de números
+// Crie dois campos numéricos e um botão “Multiplicar”.
+// Mostre o resultado da multiplicação na tela.
+
+// Exercício 6
 
 
+function multiplicar(){
+    let n1 = Number(document.getElementById("num1").value)
+    let n2 = Number(document.getElementById("num2").value)
+
+    let soma = n1 * n2
+    document.getElementById("multi").textContent = soma
+}
 
 
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Mostrar idade
+// Crie um campo para o usuário digitar a idade e um botão “Ver idade”.
+// Ao clicar, mostre a mensagem:
+
+// "Sua idade é: X anos"
+
+// Exercício 7
 
 
+function mudarIdade(){
+    document.getElementById("idadeMostrar").textContent = Number(document.getElementById("idade").value)
+}
 
 
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Alterar texto
+// Crie um parágrafo com o texto:
+// "Texto original"
+
+// Crie um botão “Alterar texto” que muda o conteúdo do parágrafo para:
+// "Texto alterado com JavaScript".
+
+// Exercício 8
+
+
+function alterarTexto(){
+    let texto = document.getElementById("texto").textContent
+    texto = textoDiferente(texto)
+    document.getElementById("texto").textContent = texto
+}
+
+function textoDiferente(texto){
+    if (texto != "Texto alterado com JavaScript"){
+        texto = "Texto alterado com JavaScript"
+    } else {
+        texto = "Texto original"
+    }
+    return texto
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Mostrar ou esconder texto
+// Crie um texto na tela e um botão “Mostrar/Esconder”.
+// Quando clicar, o texto deve desaparecer.
+// Ao clicar novamente, o texto deve aparecer novamente.
+
+// Exercício 9
+
+function sumirAparecer(){
+    let texto = document.getElementById("aparecido")
+    texto = aOUs(texto)
+    document.getElementById("aparecido").style.color = texto
+}
+
+function aOUs(texto){
+    if (texto.style.color !== "white"){
+        texto.style.color = "white"
+    } else {
+        texto.style.color = "black"
+    }
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Calculadora simples
+// Crie dois campos numéricos e quatro botões:
+
+// Somar
+
+// Subtrair
+
+// Multiplicar
+
+// Dividir
+
+// Mostre o resultado da operação na tela.
+
+// Exercício 10
+
+
+function pegarValor(){
+    let n1 = Number(document.getElementById("numero1").value)
+    let n2 = Number(document.getElementById("numero2").value)
+    n1, n2 = correcao(n1, n2)
+    return {n1, n2}
+}
+
+function correcao(n1, n2){
+    if (isNaN(n1)){
+        n1 = 0
+    }
+    if (isNaN(n2)){
+        n2 = 0
+    }
+    return n1, n2
+}
+
+function mostrarResultado(valor){
+    document.getElementById("resultado").textContent = valor
+}
+
+function adicao(){
+    let {n1, n2} = pegarValor()
+    let resultado = n1 + n2
+    return mostrarResultado(resultado)
+}
+
+function subitracao(){
+    let {n1, n2} = pegarValor()
+    let resultado = n1 - n2
+    return mostrarResultado(resultado)
+}
+
+function multiplicao(){
+    let {n1, n2} = pegarValor()
+    let resultado = n1 * n2
+    return mostrarResultado(resultado)
+}
+
+function divisao(){
+    let {n1, n2} = pegarValor()
+    let resultado = n1 / n2
+    return mostrarResultado(resultado)
+}
 
 
